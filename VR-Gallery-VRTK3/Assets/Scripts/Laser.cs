8a97cfce6e12b83b86c71ff916d06a8b2cd10627
ignasi <ignasi.gv@outlook.es>
2019-03-21 17:25:06 +0200

    Laser collision & destruction, spray test scene

--- VR-Gallery-VRTK3/Assets/Scripts/Laser.cs
+++ VR-Gallery-VRTK3/Assets/Scripts/Laser.cs
@@ -7,9 +7,11 @@ public class Laser : MonoBehaviour {
 	private float scaleTo;
 	private float speed;
 	private Rigidbody rb;
+	private BoxCollider col;
 
 	void Awake () {
 		rb = gameObject.GetComponent<Rigidbody> ();
+		col = gameObject.GetComponent<BoxCollider> ();
 		gameObject.transform.localScale = new Vector3 (0.1f, 0.1f, 0f);
 	}
 
@@ -19,14 +21,42 @@ public class Laser : MonoBehaviour {
 		StartCoroutine (ScaleUp ());
 	}
 
-	IEnumerator ScaleUp(){
-		Debug.Log ("starting scaling");
+	private void Kill(Collider collider){
+		if (collider.tag == "shape")
+		{
+			//collider.gameObject.Kill();
+		}
+		gameObject.SetActive(false);
+		Destroy(gameObject);
+	}
+
+	private void OnTriggerEnter(Collider collider){
+		StartCoroutine(ScaleDown(collider));
+	}
+
+	private IEnumerator ScaleUp(){
+		//Debug.Log ("starting scaling");
 		while (gameObject.transform.localScale.z < scaleTo) {
 			gameObject.transform.localScale += Vector3.forward * Time.deltaTime * speed;
 			Debug.Log (gameObject.transform.localScale);
 			yield return null;
 		}
 		rb.velocity = transform.forward * speed;
-		Debug.Log ("speeding");
+		//Debug.Log ("speeding");
+	}
+
+	private IEnumerator ScaleDown(Collider collider){
+		while (gameObject.transform.localScale.z > 0){
+			float factor = Time.deltaTime * speed;
+			if (gameObject.transform.localScale.z - factor >= 0)
+			{
+				gameObject.transform.localScale -= Vector3.forward * factor;
+			} else {
+				gameObject.transform.localScale = new Vector3 (0.1f, 0.1f, 0f);
+				rb.velocity = Vector3.zero;
+			}
+			yield return null;
+		}
+		Kill(collider);
 	}
 }
