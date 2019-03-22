0af0c71f8241a6c9220ae431e536f3370a165508
ignasi <ignasi.gv@outlook.es>
2019-03-22 20:11:13 +0200

    a lot of shit

---
+++ VR-Gallery-VRTK3/Assets/Scripts/SpaceShape.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShape : MonoBehaviour {
+
+	[SerializeField] private ParticleSystem ps;
+
+	public void Kill(){
+		ps = Instantiate (ps, transform.position, Random.rotation);
+		StartCoroutine (Destroy());
+		gameObject.SetActive (false);
+	}
+
+	IEnumerator Destroy(){
+		yield return new WaitForSeconds (3);
+		Destroy (ps.gameObject);
+		Destroy (gameObject);
+	}
+}
