575485e9fed27c50469c3530a8bbb74cf6abe0f6
ignasi <ignasi.gv@outlook.es>
2019-04-01 04:13:00 +0300

    rutger card destruction

---
+++ VR-Gallery-VRTK3/Assets/Materials/Shaders/NewSpriteClip.shader
@@ -0,0 +1,50 @@
+﻿Shader "Custom/NewSpriteClip" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_ClipRange ("Clip Threshold", Range(0.0, 1.0)) = 0
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma surface surf Standard fullforwardshadows
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		sampler2D _MainTex;
+
+		struct Input {
+			float2 uv_MainTex;
+		};
+
+		half _ClipRange;
+		fixed4 _Color;
+
+		// Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+		// See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+		// #pragma instancing_options assumeuniformscaling
+		UNITY_INSTANCING_BUFFER_START(Props)
+			// put more per-instance properties here
+		UNITY_INSTANCING_BUFFER_END(Props)
+
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+			// Albedo comes from a texture tinted by color
+			o.Albedo = tex2D(_MainTex, IN.uv_MainTex);
+			o.Alpha = 1;
+			
+            clip(_ClipRange - IN.uv_MainTex.y);
+			float w = clamp((_ClipRange - IN.uv_MainTex.y) * 10, 0, 1);	
+			if (_ClipRange == 1) {
+				w = 1;
+			}
+			o.Emission = lerp(_Color * 3, half4(0,0,0,0), w);
+			// Metallic and smoothness come from slider variables
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
---
+++ VR-Gallery-VRTK3/Assets/Scripts/Card.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Card : VRTK_InteractableObject {
+
+	private Transform pst;
+	private SpriteRenderer sr;
+
+	//*
+	void Start(){
+		pst = transform.GetChild(0);
+		sr = GetComponent<SpriteRenderer>();
+		pst.localPosition = Vector3.up * sr.sprite.bounds.extents.y;
+		Debug.Log(GetComponent<SpriteRenderer>().sprite.bounds.extents);
+		StartCoroutine (Kill(5f));
+	}
+	//*/
+	public override void StartUsing(VRTK_InteractUse currentUsingObject)
+	{
+		base.StartUsing(currentUsingObject);
+		pst = transform.GetChild(0);
+		sr = GetComponent<SpriteRenderer>();
+		pst.localPosition = Vector3.up * sr.sprite.bounds.extents.y;
+		Debug.Log(GetComponent<SpriteRenderer>().sprite.bounds.extents);
+		StartCoroutine (Kill(5f));
+	}
+
+	IEnumerator Kill(float duration){
+		float destPosition = -pst.transform.localPosition.y;
+		float particleSpeed = 2 * pst.transform.localPosition.y / duration;
+		float clipSpeed = 1/duration;
+		float clip = 1f;
+		pst.GetComponent<ParticleSystem>().Play();
+		pst.GetComponentInChildren<ParticleSystem>().Play();
+		//GetComponent<Rigidbody>().AddTorque(transform.up * 5f);
+		while (pst.transform.localPosition.y > destPosition){
+			clip -= Time.deltaTime * clipSpeed;
+			sr.material.SetFloat("_ClipRange", clip);
+			pst.transform.localPosition += Vector3.down * Time.deltaTime * particleSpeed;
+
+			yield return null;
+		}
+		yield return new WaitForSeconds(2f);
+		Destroy(gameObject);
+	}
+}
